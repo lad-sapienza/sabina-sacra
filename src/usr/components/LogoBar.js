32f59c0789410d93eb92b218a378fa1c39f564a1
Julian Bogdani <jbogdani@gmail.com>
2025-05-05 06:15:59 +0200

    refactor LogoBar component for improved readability and structure; enhance logo mapping and styling

--- src/usr/components/LogoBar.js
+++ src/usr/components/LogoBar.js
@@ -1,44 +1,71 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
 
 const logos = [
-  { src: "/images/logo_nextgenerationEU.png", alt: "NextGenerationEU" },
-  { src: "/images/logo_Ministero.png", alt: "Ministero dell’Università e della Ricerca" },
-  { src: "/images/logo_italiadomani.png", alt: "Italia Domani - PNRR" },
-  { src: "/images/logo_changes.png", alt: "CHANGES" },
-  { src: "/images/logo_sapienza.png", alt: "Sapienza Università di Roma" },
-];
+  {
+    src: "/images/logo_nextgenerationEU.png",
+    alt: "NextGenerationEU",
+  },
+  {
+    src: "/images/logo_Ministero.png",
+    alt: "Ministero dell’Università e della Ricerca",
+  },
+  {
+    src: "/images/logo_italiadomani.png",
+    alt: "Italia Domani - PNRR",
+  },
+  {
+    src: "/images/logo_changes.png",
+    alt: "CHANGES",
+  },
+  {
+    src: "/images/logo_sapienza.png",
+    alt: "Sapienza Università di Roma",
+  },
+]
 
-// Ridotto padding verticale (0.25rem)
+const containerStyle = {
+  backgroundColor: "#2b65af",
+  marginTop: "5rem",
+  marginBottom: "5rem",
+}
+
 const logoWrapperStyle = {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
   padding: "0.25rem 1rem", // ⬅️ solo 4px sopra e sotto
   height: "100%",
-};
+}
+
+const imgStyle = {
+  maxHeight: "100px",
+}
 
 const LogoBar = () => {
   return (
-    <Container fluid style={{ backgroundColor: "#2b65af" }} className="py-1 px-0">
+    <Container fluid style={containerStyle}>
       <Row className="align-items-center justify-content-center text-center g-0 flex-wrap">
         {logos.map((logo, idx) => (
-          <React.Fragment key={logo.alt}>
-            <Col xs={12} sm className={idx !==0 && 'border-start'}>
-              <div style={logoWrapperStyle}>
-                <img
-                  src={logo.src}
-                  alt={logo.alt}
-                  className="img-fluid d-block"
-                  style={{ maxHeight: "100px" }}
-                />
-              </div>
-            </Col>
-          </React.Fragment>
+          <Col
+            key={logo.alt}
+            xs={12}
+            sm
+            className={idx !== 0 ? "border-start" : ""}
+          >
+            <div style={logoWrapperStyle}>
+              <img
+                src={logo.src}
+                alt={logo.alt}
+                className="img-fluid d-block"
+                style={imgStyle}
+              />
+            </div>
+          </Col>
         ))}
       </Row>
     </Container>
-  );
-};
+  )
+}
 
-export default LogoBar;
+export default LogoBar
